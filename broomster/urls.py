e82b84ea03f2dde39254cefe845ddd73ebcf6455
Evgenii Antipin <evgeniyantipin@gmail.com>
2016-03-02 03:43:22 -0500

    Added quote model, added book button on top menu, added new quote.html template for quote form

--- broomster/urls.py
+++ broomster/urls.py
@@ -19,5 +19,6 @@ from pages import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index),
+    url(r'^$', views.index, name='index'),
+    url(r'^quote$', views.quote, name='quote'),
 ]
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Quote(models.Model):
+    zip = models.IntegerField(max_length=5)
+    bedrooms = models.IntegerField(max_length=2)
+    bathrooms = models.IntegerField(max_length=2)
+    hours = models.IntegerField(max_length=2)
+    date = models.DateField()
+    time = models.TimeField()
+    email = models.EmailField()
